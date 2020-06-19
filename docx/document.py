1d9decca4a1df7ea92d366217d8a6e7458de29de
bootsielon <bootsielon@gmail.com>
2020-06-19 07:18:59 -0500

    test comment

--- docx/document.py
+++ docx/document.py
@@ -1,5 +1,5 @@
 # encoding: utf-8
-
+# comment added to bootsielon's repo
 """|Document| and closely related objects"""
 
 from __future__ import absolute_import, division, print_function, unicode_literals
